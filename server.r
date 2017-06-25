4ecab047f1c892a074a3a263e44635692e7b31db
Rensa <me@rensa.co>
2017-06-25 15:41:07 +1000

    Basic interface done

--- server.r
+++ server.r
@@ -6,30 +6,32 @@ function(input, output) {
 
   # use reactive() to ensure that dataset is updated whenever
   # inout$sampleSize changes
-  dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize), ]
-  })
+  # dataset <- reactive({
+  #   diamonds[sample(nrow(diamonds), input$sampleSize), ]
+  # })
 
   # renderPlot() does the plot rendering. duh.
-  output$plot <- renderPlot({
+  # output$plot <- renderPlot({
 
-    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) +
-      geom_point()
+  #   p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) +
+  #     geom_point()
 
-    if (input$color != 'None')
-      p <- p + aes_string(color=input$color)
+  #   if (input$color != 'None')
+  #     p <- p + aes_string(color=input$color)
 
-    facets <- paste(input$facet_row, '~', input$facet_col)
-    if (facets != '. ~ .')
-      p <- p + facet_grid(facets)
+  #   facets <- paste(input$facet_row, '~', input$facet_col)
+  #   if (facets != '. ~ .')
+  #     p <- p + facet_grid(facets)
 
-    if (input$jitter)
-      p <- p + geom_jitter()
-    if (input$smooth)
-      p <- p + geom_smooth()
+  #   if (input$jitter)
+  #     p <- p + geom_jitter()
+  #   if (input$smooth)
+  #     p <- p + geom_smooth()
 
-    print(p)
+  #   print(p)
 
-  }, height = 700)
+  # }, height = 700)
 
+  output$isit_answer = renderText({'yuuuup'})
+  output$isit_comment = renderText({'it super hot'})
 }
