e821e96922aec1476e093a0bb29329d231e27146
Steefan Contractor <stefan.contractor@gmail.com>
2017-09-18 00:36:02 +1000

    deleted calcPercentiles() in app_functions.R

--- app_functions.R
+++ app_functions.R
@@ -4,9 +4,8 @@
 # app_functions.R
 
 getHistoricalObs <- function(date = Sys.Date()) {
-  # returns a dataframe of historical Tmax, Tmin and Tavg obs
-  # Get historical obs for Tmax and Tmin
-  # Data must have columns:
+  # returns a dataframe of historical Tmax, Tmin and Tavg obs for the date provided
+  # raw csv data must have columns:
   # Product.code	BOM.station.number	Year	Month	Day	Max(Min).temperature	Days.of.acumulation	Quality
   # The first two columns are not read in
   if(missing(date)) warning("Warning: Date missing. Calculating percentiles for today's date")
@@ -21,19 +20,11 @@ getHistoricalObs <- function(date = Sys.Date()) {
 }
 
 calcHistPercentiles <- function(Obs) {
-  # returns a list of Tmax, Tmin and Tavg, each element being a vector of 6 percentiles:
+  # returns a dataframe with columns Tmax, Tmin and Tavg, each row refering to the 6 percentiles:
   # 5, 10, 40, 60, 90, 95
   if(missing(Obs)) stop("Error: Missing historical observations")
   return(sapply(SydHistObs %>% select(-c(Year, Month, Day)), 
                 FUN = quantile, probs = c(0.05,0.1,0.4,0.6,0.9,0.95)))
-}
-
-calcPercentiles <- function(obsSingleVar, date) {
-  # calculates percentiles and returns a vector
-  return(quantile(obsSingleVar[which(obsSingleVar$Day == day(date) & 
-                                    obsSingleVar$Month == month(date)),4],
-           probs = c(0.05, 0.1, 0.4, 0.6, 0.9, 0.95)))
-  # the column index 4 in the above line is because Tmax/Tmin should be the 4th column
 }
 
 getCurrentObs <- function() {
