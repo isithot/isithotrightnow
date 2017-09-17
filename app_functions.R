d82fa3c86b5ce8275cd922005555b1852b73092b
Steefan Contractor <stefan.contractor@gmail.com>
2017-09-17 13:47:56 +1000

    Add getHistoricalObs() to app_functions.R and call it in app.R

--- app_functions.R
+++ app_functions.R
@@ -3,4 +3,14 @@
 
 # app_functions.R
 
-
+getHistoricalObs <- function () {
+  # Get Climatology data
+  # Use Mat's pre-calculated Tmax 90th pc data
+  SydObs.tmax90p.clim.raw <- read.csv("data/tmax90p_066062.csv", header = F,
+                                      stringsAsFactors = F)
+  names(SydObs.tmax90p.clim.raw) <- c("Month", "Day", "Tmax90p")
+  SydObs.tmin90p.clim.raw <- read.csv("data/tmin90p_066062.csv", header = F,
+                                      stringsAsFactors = F)
+  names(SydObs.tmin90p.clim.raw) <- c("Month", "Day", "Tmin90p")
+  return(list(Tmax = SydObs.tmax90p.clim.raw, Tmin = SydObs.tmin90p.clim.raw))
+}
