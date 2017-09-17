7522517ec08cdecac1fbc6b594b72e4fb6659f92
Mat Lipson <m.lipson@unsw.edu.au>
2017-09-17 16:13:59 +1000

    updated scrape30min to grep latest obs into csv file in data/hist_

--- scrape30min.sh
+++ scrape30min.sh
@@ -3,8 +3,11 @@
 # It is run through crontab, editable with:
 # sudo crontab -u shiny -e
 
-curl -o data/IDN60901.94768.axf http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.axf
-curl -o data/IDN60901.94768.json http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json
+# grep last 30min obs (line starting with '0,') into data/hist_ file
+sudo grep -o '^0,.*' data/IDN60901.94768.axf >> data/hist_IDN60901.94768.csv
+# download latest observations 
+sudo curl -o data/IDN60901.94768.axf http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.axf
+sudo curl -o data/IDN60901.94768.json http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json
 
 echo "latest observations have been scraped"
 exit
