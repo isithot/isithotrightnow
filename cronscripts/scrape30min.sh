ac12920cb8d4fd21e361ccbf8517a411aa3a3719
Ubuntu <ubuntu@ip-172-31-14-13.ap-southeast-2.compute.internal>
2017-09-23 00:53:46 +0000

    update cronscript

--- cronscripts/scrape30min.sh
+++ cronscripts/scrape30min.sh
@@ -3,11 +3,12 @@
 # It is run through crontab, editable with:
 # sudo crontab -u shiny -e
 
-# grep last 30min obs (line starting with '0,') into data/hist_ file
-grep -o '^0,.*' ../data/IDN60901.94768.axf >> ../data/hist_IDN60901.94768.csv
 # download latest observations 
-curl -o ../data/IDN60901.94768.axf http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.axf
-curl -o ../data/IDN60901.94768.json http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json
+curl -o /srv/isithotrightnow/data/IDN60901.94768.axf http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.axf
+curl -o /srv/isithotrightnow/data/IDN60901.94768.json http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json
+
+# grep last 30min obs (line starting with '0,') into data/hist_ file
+grep -o '^0,.*' /srv/isithotrightnow/data/IDN60901.94768.axf >> /srv/isithotrightnow/data/hist_IDN60901.94768.csv
 
 echo "latest observations have been scraped"
 exit
