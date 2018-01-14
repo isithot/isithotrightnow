0d2b4a4afe5fdd43656f53106a312acf40a92e5d
Ubuntu <ubuntu@ip-172-31-14-13.ap-southeast-2.compute.internal>
2018-01-14 06:37:35 +0000

    Absolute path for new cronscript

--- cronscripts/scrape30min.r
+++ cronscripts/scrape30min.r
@@ -1,4 +1,4 @@
-#!/usr/bin/R
+#!/usr/bin/Rscript
 
 # File: scrape30min.r
 # stefan contractor, mat lipson and james goldie
@@ -11,7 +11,7 @@ for (state in c("D", "N", "Q", "S", "T", "V", "W"))
   download.file(
     paste0("ftp://ftp.bom.gov.au/anon/gen/fwo/ID", state, "60920.xml"),
     destfile = paste0(
-      "data/latest/latest-",
+      "/srv/isithotrightnow/data/latest/latest-",
       switch(state, D = "nt", N = "nsw", Q = "qld", S = "sa", T = "tas",
         V = "vic", W = "wa"),
       ".xml"))
