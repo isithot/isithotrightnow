572b0f3f78ea89613819b35cf59da59e922fa661
James Goldie (Rensa) <me@rensa.co>
2018-01-14 14:54:09 +1100

    Change to scraping current obs from XML files on BOM FTP (and use R for it instead)

---
+++ cronscripts/scrape30min.r
@@ -0,0 +1,18 @@
+#!/usr/bin/R
+
+# File: scrape30min.r
+# stefan contractor, mat lipson and james goldie
+# Description: 
+# This is a script run every half hour to scrape current observations
+# It is run through crontab, editable with: crontab
+
+for (state in c("D", "N", "Q", "S", "T", "V", "W"))
+{
+  download.file(
+    paste0("ftp://ftp.bom.gov.au/anon/gen/fwo/ID", state, "60920.xml"),
+    destfile = paste0(
+      "data/latest/latest-",
+      switch(state, D = "nt", N = "nsw", Q = "qld", S = "sa", T = "tas",
+        V = "vic", W = "wa"),
+      ".xml"))
+}
