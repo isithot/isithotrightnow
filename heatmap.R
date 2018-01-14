15ac29b076e2fafabf9cddceffc836107154c491
Steefan Contractor <stefan.contractor@gmail.com>
2018-01-14 16:40:42 +1100

    created a new R file called heatmap.R that creates a heatmap of thresholds over the past 30 days and is run at midnight everyday

--- heatmap.R
+++ heatmap.R
@@ -1,2 +1,6 @@
 # A heatmap of thresholds over the past 30 days
 
+dummy.data <- rnorm(30, mean = 0.60, sd = 0.4)
+dummy.data[which(dummy.data > 1.0)] <- 1.0
+
+image(matrix(dummy.data, ncol = 1), xaxt = "n", yaxt = "n")
