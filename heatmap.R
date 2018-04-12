afa22400d44bdd97d7623a1684ae60468f4b0773
Mat Lipson <m.lipson@unsw.edu.au>
2018-04-12 13:20:32 +1000

    updated resolution for heatmap

--- heatmap.R
+++ heatmap.R
@@ -69,8 +69,8 @@ for (d in 95:length(dates)) {
 }
 
 for (i in 1:length(station_set)) {
-  png(paste0("www/output/",station_set[[i]]$id,"/heatmap.png"), width = 1200, height = 530)
-  par(mar = c(0,3,5,0.5) + 0.1, bg = NA, family = "Roboto Condensed")
+  png(paste0("www/output/",station_set[[i]]$id,"/heatmap.png"), width = 2400, height = 1060)
+  par(mar = c(0.8,5,8,0.5) + 0.1, bg = NA, family = "Roboto Condensed")
   layout(mat = matrix(c(1,2), byrow = T, ncol = 2), widths = c(1, 0.075))
   cols <- rev(c('#b2182b','#ef8a62','#fddbc7','#f7f7f7','#d1e5f0','#67a9cf','#2166ac'))
   breaks <- c(0,0.05,0.2,0.4,0.6,0.8,0.95,1)
@@ -83,14 +83,14 @@ for (i in 1:length(station_set)) {
         xaxt = "n", yaxt ="n",
         xlab = "", ylab = "", breaks = breaks, col = cols)
   title(paste(station_set[[i]]$label, "percentiles for 2018"), 
-        cex.main = 2, line = 3.5, col = "#333333")
-  axis(side = 3, at = seq(1, 31),lwd.ticks = 0, )
-  axis(side = 2, at = seq(12, 1), labels = month.names, las = 2, lwd.ticks = 0)
-  text(expand.grid(1:31, 12:1), labels = station_set[[i]]$percentileHeatmap_array)
-  par(mar = c(3,0,5,30) + 0.1, bg = NA)
+        cex.main = 4, line = 5.5, col = "#333333")
+  axis(side = 3, at = seq(1, 31), lwd.ticks = 0, cex.axis = 2.3)
+  axis(side = 2, at = seq(12, 1), labels = month.names, las = 2, lwd.ticks = 0, cex.axis = 2.3)
+  text(expand.grid(1:31, 12:1), labels = station_set[[i]]$percentileHeatmap_array, cex = 2.3)
+  par(mar = c(0.8,0,8,30) + 0.1, bg = NA)
   colbar <- c(cols[1], rep(cols[2], 3), rep(cols[3], 4),rep(cols[4], 4), rep(cols[5], 4), rep(cols[6], 3), cols[7])
   colkey(col = colbar, clim = c(0, 1), at = breaks, side = 4, width = 6,
-         labels = paste(breaks*100))
+         labels = paste(breaks*100), cex.axis = 2.3)
   dev.off()
 }
 
