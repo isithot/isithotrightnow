c7811463fe49117c19e128bd26989335b642bac5
Mat Lipson <m.lipson@unsw.edu.au>
2018-12-08 14:36:55 +1100

    updated records.py

--- databackup/records.py
+++ databackup/records.py
@@ -7,32 +7,29 @@ import pandas as pd
 pd.set_option('display.width', 150)
 
 year = '2018'
-month = '10'
+month = '12'
 sday = 1
-eday = 27
-
+eday = 7
 
 # read in csv of site monthtly data from eg. http://www.bom.gov.au/climate/dwo/IDCJDW4020.latest.shtml, then click on 'plain text version'.
 # canberra = pd.read_csv('IDCJDW2801.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=8,names=['day','tmin','tmax'])
 alice  = pd.read_csv('IDCJDW8002.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=8,names=['day','tmin','tmax'])
-darwin = pd.read_csv('IDCJDW8014.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=6,names=['day','tmin','tmax'])
-richmond = pd.read_csv('IDCJDW2119.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=9,names=['day','tmin','tmax'])
+s_alice = (alice,'015590')
+# darwin = pd.read_csv('IDCJDW8014.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=6,names=['day','tmin','tmax'])
+# s_darwin = (darwin,'014015')
+# richmond = pd.read_csv('IDCJDW2119.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=9,names=['day','tmin','tmax'])
+# s_richmond = (richmond,'067105')
+adelaide = pd.read_csv('IDCJDW5002.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=8,names=['day','tmin','tmax'])
+s_adelaide = (adelaide,'023090')
+laverton = pd.read_csv('IDCJDW3043.%s%s.csv' %(year,month),header=None,usecols =[1,2,3],skiprows=7,names=['day','tmin','tmax'])
+s_laverton = (laverton,'087031')
 
-# old formats
-# darwin 		= pd.read_csv('Darwin_014015.csv',	header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# alice 		= pd.read_csv('Alice_015590.csv',	header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# sydney 		= pd.read_csv('Sydney_066062.csv',	header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# richmond 	= pd.read_csv('Richmond_067105.csv',header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# brisbane 	= pd.read_csv('Brisbane_040842.csv',header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# adelaide 	= pd.read_csv('Adelaide_023090.csv',header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# hobart 		= pd.read_csv('Hobart_094029.csv',	header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# melbourne 	= pd.read_csv('Melbourne_087031.csv',header=None,usecols=[0,2,3],names=['day','tmin','tmax'])
-# perth 		= pd.read_csv('Perth_009021.csv',	header=None,usecols =[0,2,3],names=['day','tmin','tmax'])
-# sites = [darwin,alice,sydney,canberra,richmond,brisbane,adelaide,hobart,melbourne,perth]
+
+
 
 # list site (name,ID) tupple
 # sites = [(canberra,70351)]
-sites = [(alice,'015590'),(darwin,'014015'),(richmond,'067105')]
+sites = [s_alice,s_adelaide,s_laverton]
 
 # for each day in month, loop through each site to replace values in template frame with csv values
 for day in range(sday,eday+1):
@@ -44,7 +41,7 @@ for day in range(sday,eday+1):
 		newtmin = site[0].loc[site[0].day=='2018-%s-%s' %(month,day),'tmin'].values[0]
 		newtemplate.loc[newtemplate.station_id==site[1],'tmax']    = newtmax
 		newtemplate.loc[newtemplate.station_id==site[1],'tmin']    = newtmin
-		newtemplate.loc[newtemplate.station_id==site[1],'tmax_dt'] = '2018-%s-%sT00:00:00Z' %(month,str(day).zfill(2))
-		newtemplate.loc[newtemplate.station_id==site[1],'tmin_dt'] = '2018-%s-%sT00:00:00Z' %(month,str(day).zfill(2))
+		newtemplate.loc[newtemplate.station_id==site[1],'tmax_dt'] = '2018-%s-%sT06:00:00Z' %(month,str(day).zfill(2))
+		newtemplate.loc[newtemplate.station_id==site[1],'tmin_dt'] = '2018-%s-%sT18:00:00Z' %(month,str(day-1).zfill(2))
 	# for each day, save to csv
 	newtemplate.to_csv('18%s%s-all.csv' %(month,str(day).zfill(2)),index=False)
