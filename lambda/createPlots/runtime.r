a4da76e88fd6331cfe5ca25b5dbb7007b3ddb00c
James Goldie <james@dicenerd.app>
2023-04-30 11:30:59 +1000

    WIP plot cleanup

--- lambda/createPlots/runtime.r
+++ lambda/createPlots/runtime.r
@@ -2,6 +2,8 @@ library(ggplot2)
 library(tibble)
 library(aws.s3)
 
+source("util.r")
+
 # defaultFunc: just prints a message. here as a default so that you can verify
 # your lambda config (and to make it clear if you've forgotten to override
 # the entrypoint)
@@ -11,6 +13,93 @@ defaultFunc <- function() {
     "If you're configuring a new Lambda function, you should override the",
     "Lambda's entrypoint to be the name of one of the other functions in this",
     "file.")))
+}
+
+createTimeseriesPlot <- function() {
+
+  # things we need:
+  # - HistObs: data frame of historical obs, including cols:
+  #    - Year, Month, Day, Tmax, Tmin, Tavg, Date, current.date, Tavg.now
+  # - histPercentiles: array? dims include 5%, 95%, Tavg
+  # - this_station: cols record_start_date, label
+
+  TS.plot <-
+    ggplot(data = HistObs) +
+    aes(x = Date, y = Tavg) +
+    # james - i don't understand what these two geoms are for
+    # geom_line(size = 0.0, colour = '#CCCCCC') + 
+    # geom_point(size = rel(1.5), colour = "#999999", alpha = 0.5) +
+    geom_smooth(method = lm, se = FALSE, col = "gray60", size = 0.5) + 
+    geom_point(
+      aes(x = current.date, y = Tavg.now),
+      colour = iihrn_colour,
+      size = rel(5)) +
+    geom_hline(
+      aes(yintercept = histPercentiles["95%", "Tavg"]),
+      linetype = 2,
+      alpha = 0.5) +
+    geom_hline(
+      aes(yintercept = histPercentiles["5%", "Tavg"]),
+      linetype = 2,
+      alpha = 0.5) +
+    scale_x_date(
+      date_breaks = "20 years",
+      date_labels = "%Y") +
+    scale_y_continuous(
+      # james - the limits can be handled by the `expand` argument, but the
+      # default is probably fine
+      breaks = seq(0, 100, by = 5)) +
+    annotate_text_iihrn(
+      x = current.date,
+      y = Tavg.now,
+      vjust = -1.5,
+      label = "TODAY",
+      colour = iihrn_colour) +
+    annotate_text_iihrn(
+      x = current.date,
+      y = Tavg.now,
+      vjust = 2.5,
+      label = paste0(Tavg.now, "°C"), hl = TRUE) +
+    # james - perhaps we should consider replacing the dotted lines with blocks
+    # of colour that are the same as our category colours?
+    annotate_text_iihrn(
+      x = -Inf,
+      y = histPercentiles["95%", "Tavg"],
+      label = paste0(
+        "95th percentile: ",
+        round(histPercentiles["95%", "Tavg"], 1),
+        "°C"),
+      hjust = "inward",
+      vjust = -0.5) +
+    annotate_text_iihrn(
+      x = -Inf,
+      y = histPercentiles["5%", "Tavg"],
+      label = paste0(
+        "5th percentile: ",
+        round(histPercentiles["5%", "Tavg"], 1),
+        "°C"),
+      hjust = "inward",
+      vjust = -0.5) +
+    annotate_text_iihrn(
+      x = -Inf,
+      y = histPercentiles["50%", "Tavg"],
+      label = paste0("Trend: +", round(trend * 365 * 100, 1), "°C/ century"),
+      hjust = "inward",
+      vjust = -0.5) +
+    theme_iihrn() +
+    theme(
+      axis.line = element_line(),
+      axis.text.y = element_text(face = "bold"),
+      axis.title.y = element_text(face = "bold", size = rel(0.8))
+    ) +
+    labs(
+      title = paste0(
+        this_station[["label"]],
+        " daily average temperatures\nfor the two weeks around ",
+        format(current.date, format = "%d %B", tz = this_station[["tz"]])),
+      x = NULL,
+      y = "Daily average temperature (°C)")
+
 }
 
 # createTestPlot: our "test" plotting function. generates a plot from random
---
+++ lambda/createPlots/util.r
@@ -0,0 +1,50 @@
+
+# iihrn_colour: highlight colour
+iihrn_colour <- "firebrick"
+base_colour <- "#333333"
+
+#' Define some sensible defaults for text annotations
+#' 
+#' @param size: The size of the text. Defaults to 50% of the base size.
+#' @param highlight: If true, use the highlight text colour.
+#' @param ...: Other arguments passed to annotate.
+annotate_text_iihrn <- function(size = rel(0.5),
+  highlight = c(FALSE, TRUE), ...) {
+  annotate(
+    geom = "text",
+    family = "Roboto Condensed",
+    fontface = "bold",
+    colour = ifelse(highlight, iihrn_colour, base_colour),
+    size = size,
+    ...)
+}
+
+#' Common theme elements for all Is is hot right now plots
+#' 
+#' @param base_family: The base font family to use.
+#' @param base_size: The base font size to use.
+theme_iihrn <- function(base_family = "Roboto Condensed", base_size = 20) {
+  theme_bw(base_family = base_family, base_size = base_size) +
+  theme(
+    # transparent background
+    plot.background = element_rect(fill = "transparent", colour = NA),
+    panel.background = element_rect(fill = "transparent", colour = NA),
+    # no axis gridlines
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    # consistent text
+    plot.title = element_text(
+      face = "bold",
+      color = base_colour,
+      size = rel(0.9),
+      hjust = 0.5),
+    axis.text = element_text(face = "bold"),
+    axis.title = element_text(face = "bold", size = rel(0.8)),
+    axis.ticks.y = element_blank()
+  )
+}
+
+# for dist plot:
+# axis.title.y = element_blank(),
+# axis.text.y = element_blank(),
