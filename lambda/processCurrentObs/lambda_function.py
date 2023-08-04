21059b715085e3c2a014d98dc21001f55f6d8294
Steefan Contractor <stefan.contractor@gmail.com>
2023-08-04 17:02:38 +1000

    Update processCurrentObs lamda function.
    Fix all AWS errors

--- lambda/processCurrentObs/lambda_function.py
+++ lambda/processCurrentObs/lambda_function.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import json
 import boto3
+import os
+from datetime import datetime
+from pytz import timezone
+
 
 def get_current_obs(req_station_id, fileid):
     # Returns a data frame with the max and min temps reported by the station
@@ -10,6 +14,7 @@ def get_current_obs(req_station_id, fileid):
     df = df[df['station_id'] == req_station_id][['tmax', 'tmin']]
     print(df)
     return df
+
 
 def calc_hist_percentiles(Obs):
     # Returns a data frame with columns Tmax, Tmin and Tavg, each row referring to the 6 percentiles:
@@ -22,15 +27,16 @@ def calc_hist_percentiles(Obs):
     result.columns = ['Tmax', 'Tmin', 'Tavg']
     return result
 
+
 def determine_answer_and_comment(category_now):
     switcher_answer = {
-    'bc': 'Hell no!',
-    'rc': 'No!',
-    'c': 'Nope',
-    'a': 'Not really',
-    'h': 'Yup',
-    'rh': 'Yeah!',
-    'bh': 'Hell yeah!'
+        'bc': 'Hell no!',
+        'rc': 'No!',
+        'c': 'Nope',
+        'a': 'Not really',
+        'h': 'Yup',
+        'rh': 'Yeah!',
+        'bh': 'Hell yeah!'
     }
     isit_answer = switcher_answer.get(category_now, 'Invalid category')
 
@@ -47,112 +53,156 @@ def determine_answer_and_comment(category_now):
 
     return isit_answer, isit_comment
 
+
 def bin_obs(tavg_now, hist_percentiles):
-    return pd.cut(
-        Tavg_now,
+    return pd.cut([tavg_now],
         bins=[
             -100,
-            *hist_percentiles.loc[hist_percentiles.index != "50%", "Tavg"],
+            *hist_percentiles.loc[hist_percentiles.index != 0.5, "Tavg"],
             100
         ],
         labels=["bc", "rc", "c", "a", "h", "rh", "bh"],
         include_lowest=True,
         right=False
-        ).astype(str)
-        # The -100 and 100 allow us to have the lowest and highest bins
+    ).astype(str)[0]
+    # The -100 and 100 allow us to have the lowest and highest bins
+
 
 def ECDF(data):
     """Compute ECDF for a one-dimensional array of measurements."""
     # Sort the data in ascending order
     sorted_data = np.sort(data)
-    
+
     # Calculate the y-values for the ECDF
     n = len(data)
-    y_values = np.arange(1, n+1) / n
-    
+    y_values = np.arange(1,n+1) / n
+
     # Define the ECDF function
     def ecdf_function(x):
         """Return the value of the ECDF for a given x."""
         # Find the index of the first element in the sorted data greater than or equal to x
-        index = np.searchsorted(sorted_data, x, side='right')
-        
+        index = np.searchsorted(sorted_data, x, side='left')
+
         # Return the y-value for that index
-        return y_values[index]
-    
+        return y_values[index-1]
+
     # Return the ECDF function
     return ecdf_function
 
 
 def tavg_hist_percentile(hist_tavg, tavg_now):
     ecdf = ECDF(hist_tavg)
-    return 100 * np.round(ecdf(tavg_now), 2)
+    return 0. if tavg_now <= min(hist_tavg) else 100. if tavg_now > max(hist_tavg) else 100 * np.round(ecdf(tavg_now), 3)
+
+def download_from_aws(s3_fpath):
+
+    s3 = boto3.client('s3')
+    bucket_name = 'isithot-data'
+
+    fname = os.path.basename(s3_fpath)
+    local_file_path = f'/tmp/{fname}'
+
+    try:
+        # Get the object from S3 bucket
+        response = s3.get_object(Bucket=bucket_name, Key=s3_fpath)
+
+        # Save the object to local file
+        with open(local_file_path, 'wb') as f:
+            f.write(response['Body'].read())
+
+        print(f"File saved to {local_file_path}")
+
+        return local_file_path
+
+    except Exception as e:
+        print(f"Error getting S3 object: {e}")
+        return None
+
+def upload_to_aws(local_file, s3_file):
+
+    s3 = boto3.client('s3')
+    bucket_name = 'isithot-data'
+
+    try:
+        s3.upload_file(local_file, bucket_name, s3_file)
+        url = s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': s3_file
+            },
+            ExpiresIn=24 * 3600
+        )
+
+        print("Upload Successful", url)
+        return url
+    except FileNotFoundError:
+        print("The file was not found")
+        return None
 
 def lambda_handler(event, context):
+
+    station_id, tz, lat, lon, tmax_now, tmax_dt, tmin_now, tmin_dt = event.values()
+    print(f"\n\nBeginning analysis: {station_id}")
+
     # Load station data from locations.json
-    loc_s3_fpath = f'sandbox/locations.json'
+    loc_s3_fpath = f'1-datasources/locations.json'
     loc_local_fpath = download_from_aws(loc_s3_fpath)
     with open(loc_local_fpath) as f:
         station_set = json.load(f)
+    this_station = [s for s in station_set if s['id'] == station_id][0]
 
-    # Loop over each station in station_set
-    for this_station in station_set:
-        print(f"\n\nBeginning analysis: {this_station['label']}")
+    # Create directory for output files
+    # output_dir = f"output/{station_id}"
+    # os.makedirs(output_dir, exist_ok=True)
 
-        # Create directory for output files
-        output_dir = f"output/{this_station['id']}"
-        os.makedirs(output_dir, exist_ok=True)
+    # Get current date and time
+    current_date_time = datetime.now(timezone(tz))
+    current_date = current_date_time.date()
 
-        # Get current date and time
-        current_date_time = datetime.now(timezone(this_station['tz']))
-        current_date = current_date_time.date()
+    # Calculate tavg from current tmax and tmin
+    tavg_now = (tmax_now + tmin_now) / 2.0
+    print(f"Updating station: {station_id}")
+    print(f"Tavg.now = {tavg_now}")
 
-        # Get current max and min temperatures for this_station
-        obs_filename = 'data/latest/latest-all.csv'
-        curr_obs_df = get_current_obs(this_station['id'], obs_filename)
-        tmax_now = curr_obs_df['tmax'].values[0]
-        tmin_now = curr_obs_df['tmin'].values[0]
-        tavg_now = (tmax_now + tmin_now) / 2.0
-        print(f"Updating station: {this_station['label']}")
-        print(f"Tavg.now = {tavg_now}")
-        
-        # Calculate percentiles of historical data
-        # Read historical tmin obs from s3
-        s3_fpath = f"sandbox/historical_{station_id}.txt"
-        local_fpath = download_from_aws(s3_fpath)
-        hist_obs = pd.read_csv(local_fpath,
-                                na_values=["", " ", "NA"])
-        hist_percentiles = calc_hist_percentiles(hist_obs)
+    # Calculate percentiles of historical data
+    # Read historical tmin obs from s3
+    s3_fpath = f"2-processed/historical_{station_id}.txt"
+    local_fpath = download_from_aws(s3_fpath)
+    hist_obs = pd.read_csv(local_fpath,
+                           na_values=["", " ", "NA"])
+    hist_percentiles = calc_hist_percentiles(hist_obs)
 
-        # Determine category of current temperature based on percentiles
-        category_now = bin_obs(tavg_now, hist_percentiles)
+    # Determine category of current temperature based on percentiles
+    category_now = bin_obs(tavg_now, hist_percentiles)
 
-        # Determine answer and comment based on category
-        isit_answer, isit_comment = determine_answer_and_comment(category_now)
+    # Determine answer and comment based on category
+    isit_answer, isit_comment = determine_answer_and_comment(category_now)
 
-        # Calculate percentage of historical data that is lower than current temperature
-        average_percent = tavg_hist_percentile(hist_obs['Tavg'], tavg_now)
+    # Calculate percentage of historical data that is lower than current temperature
+    average_percent = tavg_hist_percentile(hist_obs['Tavg'], tavg_now)
 
-        # Save updated stats to file
-        # Create stats dictionary
-        stats_dict = {}
-        stats_dict['isit_answer'] = isit_answer if isit_answer else None
-        stats_dict['isit_comment'] = isit_comment if isit_comment else None
-        stats_dict['isit_maximum'] = tmax_now if tmax_now else None
-        stats_dict['isit_minimum'] = tmin_now if tmin_now else None
-        stats_dict['isit_current'] = tavg_now if tavg_now else None
-        stats_dict['isit_average'] = average_percent if average_percent else None
-        stats_dict['isit_name'] = this_station['name'] if 'name' in this_station else None
-        stats_dict['isit_label'] = this_station['label'] if 'label' in this_station else None
-        if 'record_start' in this_station and 'record_end' in this_station:
-            stats_dict['isit_span'] = f"{this_station['record_start']} - {this_station['record_end']}"
-        else:
-            stats_dict['isit_span'] = None
+    # Save updated stats to file
+    # Create stats dictionary
+    stats_dict = {}
+    stats_dict['isit_answer'] = isit_answer if isit_answer else None
+    stats_dict['isit_comment'] = isit_comment if isit_comment else None
+    stats_dict['isit_maximum'] = tmax_now if tmax_now else None
+    stats_dict['isit_minimum'] = tmin_now if tmin_now else None
+    stats_dict['isit_current'] = tavg_now if tavg_now else None
+    stats_dict['isit_average'] = average_percent if average_percent else None
+    stats_dict['isit_name'] = this_station['name'] if 'name' in this_station else None
+    stats_dict['isit_label'] = this_station['label'] if 'label' in this_station else None
+    if 'record_start' in this_station and 'record_end' in this_station:
+        stats_dict['isit_span'] = f"{this_station['record_start']} - {this_station['record_end']}"
+    else:
+        stats_dict['isit_span'] = None
 
-        # Convert dictionary to JSON string
-        stats_json = json.dumps(stats_dict)
+    # Convert dictionary to JSON string
+    stats_json = json.dumps(stats_dict)
 
-        file_path = f'/tmp/stats_{stationid}.json'
-        # Write the stats dictionary to the JSON file
-        with open(file_path, "w") as f:
-            json.dump(stats_dict, f)
-        upload_to_aws(file_path, f'sandbox/stats_{stationid}.json')
+    file_path = f'/tmp/stats_{station_id}.json'
+    # Write the stats dictionary to the JSON file
+    with open(file_path, "w") as f:
+        json.dump(stats_dict, f)
+    upload_to_aws(file_path, f'www/stats/stats_{station_id}.json')
